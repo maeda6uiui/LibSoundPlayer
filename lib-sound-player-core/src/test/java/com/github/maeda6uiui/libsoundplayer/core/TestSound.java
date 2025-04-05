e23d4623c38ac4c1f91dd4246e1028e4c2a703fb
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2025-04-05 19:36:24 +0900

    Add test code

---
+++ lib-sound-player-core/src/test/java/com/github/maeda6uiui/libsoundplayer/core/TestSound.java
@@ -0,0 +1,40 @@
+package com.github.maeda6uiui.libsoundplayer.core;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestSound {
+    private Sound sound;
+
+    @BeforeEach
+    public void loadSound() {
+        assertDoesNotThrow(() -> {
+            sound = new Sound("../Data/Op23.mp3");
+        });
+    }
+
+    @Test
+    public void testFileNotFound() {
+        assertThrows(FileNotFoundException.class, () -> {
+            sound = new Sound("example.mp3");
+        });
+    }
+
+    @Test
+    public void testSetSpeed() {
+        assertEquals(1.0f, sound.getSpeed());
+        sound.setSpeed(1.5f);
+        assertEquals(1.5f, sound.getSpeed());
+    }
+
+    @Test
+    public void testSetVolume() {
+        assertEquals(1.0f, sound.getVolume());
+        sound.setVolume(1.5f);
+        assertEquals(1.5f, sound.getVolume());
+    }
+}
